14b05ffca6af3a040ba72d3d89d0a05f80d1612c
Gav-MN <236774@shakopeeschools.org>
2025-10-28 09:53:14 -0500

    Update 125_Gavin.py
    Renamed spot 0–1 to cor 0–1, added user input for difficulty and written response.

--- 1.2.5 - Shall We Play a Game/125_Gavin.py
+++ 1.2.5 - Shall We Play a Game/125_Gavin.py
@@ -6,6 +6,7 @@ turtle_colors = ["red", "red", "red"]
 turtle_shapes = ["square", "square", "square"]
 cups = []
 hori = -150
+font_setup = ("Arial", 20, "normal")
 
 #---------Shape creation---------------
 ball = trtl.Turtle("circle")
@@ -18,18 +19,24 @@ for s in turtle_shapes:
   new_color = turtle_colors.pop()
   cup.color(new_color)
   cup.penup()
+  cup.speed(0)
   cups.append(cup)
   cup.goto(hori, 100)
   hori = hori + 150
   cup.shapesize(5)
 
-spot0 = cups[0]
-spot1 = cups[1]
-spot2 = cups[2]
+cor0 = cups[0]
+cor1 = cups[1]
+cor2 = cups[2]
+ballcor = ball
 
+difficulty = trtl.textinput("Difficulty", "Choose your difficulty (1, 2, 3)")
 
-
-
+if difficulty == "1":
+  trtl.penup()
+  trtl.hideturtle()
+  trtl.goto(-90, 200)
+  trtl.write("Easy mode activated", font=font_setup)
 
 
 

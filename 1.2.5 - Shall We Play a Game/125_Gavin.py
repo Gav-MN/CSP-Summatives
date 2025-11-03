1e91bb8ac511d6f49b1b1e6837622bedee688138
Gav-MN <236774@shakopeeschools.org>
2025-11-03 12:31:15 -0600

    removed speed 4 from easy list because it is too fast. added e, n, and h as new turtles to write choices. reorganized code. implemented e, n, and h to ask questions and implemented areas to click that match the question size. reuse e and n for want to play again. stoped repetitive code by making definitions.
    removed speed 4 from easy list because it is too fast. added e, n, and h as new turtles to write choices. reorganized code. implemented e, n, and h to ask questions and implemented areas to click that match the question size. reuse e and n for want to play again. stoped repetitive code by making definitions.

--- 1.2.5 - Shall We Play a Game/125_Gavin.py
+++ 1.2.5 - Shall We Play a Game/125_Gavin.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 #---------Screen Setup---------------
-easy = [1,2,3,4]
+easy = [1,2,3]
 normal = [1,2,3,4,5,6,8,9,10]
 hard = [0,7,10,999]
 turtle_colors = ["red", "red", "red"]
@@ -13,6 +13,11 @@ cups = []
 hori = -150
 font_setup = ("Arial", 20, "normal")
 loop = 1
+e =  trtl.Turtle()
+n =  trtl.Turtle()
+h =  trtl.Turtle()
+difficulty_chosen = False
+wn = trtl.Screen()
 #---------Shape creation---------------
 ball = trtl.Turtle("circle")
 ball.color("blue")
@@ -29,74 +34,86 @@ for s in turtle_shapes:
   cup.goto(hori, 100)
   hori = hori + 150
   cup.shapesize(5)
+#---------Question creation---------------
+e.penup()
+e.hideturtle()
+e.goto(-90, 300)
+e.write("Activate Easy Mode", font=font_setup)
+n.penup()
+n.hideturtle()
+n.goto(-90, 250)
+n.write("Activate Normal Mode", font=font_setup)
+h.penup()
+h.hideturtle()
+h.goto(-90, 200)
+h.write("Activate Hard Mode", font=font_setup)
+#-----game functions-------
+def on_click(x, y):
+    global difficulty_chosen
+    if difficulty_chosen:
+        return
+    if -90 <= x <= 90 and 300 <= y <= 320:
+        difficulty_chosen = "Easy"
+    elif -90 <= x <= 90 and 250 <= y <= 270:
+        difficulty_chosen = "Normal"
+    elif -90 <= x <= 90 and 200 <= y <= 220:
+        difficulty_chosen = "Hard"
+    else:
+        return
+    e.undo(); n.undo(); h.undo()
+    start_game(difficulty_chosen)
 
-pos0 = cups[0].pos()
-pos1 = cups[1].pos()
-pos2 = cups[2].pos()
+wn.onclick(on_click)
 
-difficulty = trtl.textinput("Difficulty", "Choose your difficulty (1, 2, 3)")
-trtl.penup()
-trtl.hideturtle()
-trtl.goto(-90, 200)
+def start_game(mode):
+    setup()
+    game()
 
-if difficulty == "1":
-  trtl.write("Easy mode activated", font=font_setup)
-  cups[0].speed(rand.choice(easy))
-  cups[1].speed(rand.choice(easy))
-  cups[2].speed(rand.choice(easy))
+def setup():
+  global pos0, pos1, pos2
+  cups[0].speed(1)
+  cups[1].speed(1)
+  cups[2].speed(1)
   cups[0].goto(-150, 0)
   cups[1].goto(0, 0)
   cups[2].goto(150, 0)
-  while 1==1:
-    if loop == 11:
-      break
-    else:
-      loop += 1
-      cups[0].speed(rand.choice(easy))
-      cups[1].speed(rand.choice(easy))
-      cups[2].speed(rand.choice(easy))
-      rand.choice()
-elif difficulty == "2":
-  trtl.write("Normal mode activated", font=font_setup)
-  cups[0].speed(rand.choice(normal))
-  cups[1].speed(rand.choice(normal))
-  cups[2].speed(rand.choice(normal))
-  cups[0].goto(-150, 0)
-  cups[1].goto(0, 0)
-  cups[2].goto(150, 0)
-  while 1==1:
-    if loop == 11:
-      break
-    else:
-      loop += 1
-      cups[0].speed(rand.choice(normal))
-      cups[1].speed(rand.choice(normal))
-      cups[2].speed(rand.choice(normal))
-elif difficulty == "3":
-  trtl.write("Hard mode activated", font=font_setup)
-  cups[0].speed(rand.choice(hard))
-  cups[1].speed(rand.choice(hard))
-  cups[2].speed(rand.choice(hard))
-  cups[0].goto(-150, 0)
-  cups[1].goto(0, 0)
-  cups[2].goto(150, 0)
-  while 1==1:
-    if loop == 11:
-      break
-    else:
-      loop += 1
-      cups[0].speed(rand.choice(hard))
-      cups[1].speed(rand.choice(hard))
-      cups[2].speed(rand.choice(hard))
+  pos0 = cups[0].pos()
+  pos1 = cups[1].pos()
+  pos2 = cups[2].pos()
+
+def show_play_again():
+    e.write("Want To Play Again?", font=font_setup)
+    n.write("Want To End?", font=font_setup)
+
+    wn.onclick(play_again)
+
+def play_again(x, y):
+    if -90 <= x <= 90 and 300 <= y <= 320:
+        os.execv(sys.executable, ['python'] + sys.argv)
+    elif -90 <= x <= 90 and 250 <= y <= 270:
+        exit()
+
+def game():
+  global pos0, pos1, pos2
+  if difficulty_chosen == "Easy":
+    for _ in range(5):
+        cups[0].speed(rand.choice(easy))
+        cups[1].speed(rand.choice(easy))
+        cups[2].speed(rand.choice(easy))
+        cups[0].goto(pos2)
+        cups[2].goto(pos0)
+  elif difficulty_chosen == "Normal":
+    for _ in range(10):
+        cups[0].speed(rand.choice(normal))
+        cups[1].speed(rand.choice(normal))
+        cups[2].speed(rand.choice(normal))
+  elif difficulty_chosen == "Hard":
+    for _ in range(20):
+        cups[0].speed(rand.choice(hard))
+        cups[1].speed(rand.choice(hard))
+        cups[2].speed(rand.choice(hard))
+  show_play_again()
 
 
-pa = trtl.textinput("Play Again", "Would you like to play again? (y/n)")
-
-if pa == "y":
-  os.execv(sys.executable, ['python'] + sys.argv)
-else:
-  exit()
-
-wn = trtl.Screen()
 wn.mainloop()
- 
+
